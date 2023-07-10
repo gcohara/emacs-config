0a9d77864a5b81e3fc8bdf1c4a1d012154b723bc
George O'Hara <georgeohara92@gmail.com>
2023-07-10 12:23:35 +0100

    refile targets

--- init.el
+++ init.el
@@ -41,7 +41,11 @@
    org-enforce-todo-dependencies t
    org-blank-before-new-entry '((heading . nil) (plain-list-item . nil))
    org-refile-use-outline-path 'file
-   org-refile-targets '(("repeated.org" :maxlevel . 1))
+   org-refile-targets '(("repeated.org" :maxlevel . 1)
+                        ("diary.org" :maxlevel . 1)
+                        ("!todo.org" :maxlevel . 1)
+                        ("zbacklog.org" :maxlevel . 1)
+                        ("projects.org" :maxlevel . 1))
    org-clock-mode-line-total 'current))
 
   ;; (setq org-blank-before-new-entry '(heading . nil))
